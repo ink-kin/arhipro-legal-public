f7706f7965571d2b78b0d586efd7aa237af589ff
ILya N Klishin <kin@csadv.ru>
2025-08-29 17:54:51 +0300

    first commit

---
+++ app/app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  cover: '/cover.jpg',
+  socials: {
+    telegram: 'https://t.me/agagafilm',
+    github: 'ink-kin/content-wind-gagarinov',
+  },
+  icon: {
+    aliases: {
+      'dark-mode': 'lucide-moon',
+      'light-mode': 'lucide-sun',
+    },
+  },
+})
